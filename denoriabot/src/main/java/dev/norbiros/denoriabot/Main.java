9be9a63288274aebb01e989640f18c380db6f44d
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-03-02 19:12:52 +0000

    Created config system

--- denoriabot/src/main/java/dev/norbiros/denoriabot/Main.java
+++ denoriabot/src/main/java/dev/norbiros/denoriabot/Main.java
@@ -1,12 +1,18 @@
 package dev.norbiros.denoriabot;
 
+import java.util.*;
+
+import org.bukkit.configuration.file.FileConfiguration
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class YourPluginName extends JavaPlugin {
+public class Main extends JavaPlugin {
     
     @Override
     public void onEnable() {
+      this.saveDefaultConfig();
+      System.out.println("TOKEN:" );
+      System.out.println(this.getConfig().getString("token"));
     }
 
     @Override
