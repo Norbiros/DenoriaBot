2ebc2787d9aa52973fe4e047d705dd582366e9ec
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-03-02 15:45:44 +0000

    Created minecraft plugin files (using maven)

--- README.md
+++
@@ -1,3 +0,0 @@
-# Denoria Bot
-This is new dsicord bot for `mc.denoria.pl` server!
-Bot now is in very early state.
---
+++ denoriabot/src/main/java/dev/norbiros/denoriabot/Main.java
@@ -0,0 +1,15 @@
+package dev.norbiros.denoriabot;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class YourPluginName extends JavaPlugin {
+    
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
