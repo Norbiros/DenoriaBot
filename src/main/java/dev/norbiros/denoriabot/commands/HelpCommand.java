b60e01adb768bd992cbe476cedffabf10207fc17
Norbiros <89837724+Norbiros@users.noreply.github.com>
2022-03-10 16:15:32 +0000

    Embed in help command.

--- src/main/java/dev/norbiros/denoriabot/commands/HelpCommand.java
+++ src/main/java/dev/norbiros/denoriabot/commands/HelpCommand.java
@@ -7,6 +7,9 @@ import net.dv8tion.jda.api.entities.MessageChannel;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
+import net.dv8tion.jda.api.EmbedBuilder;
+import java.awt.Color;
+
 public class HelpCommand extends ListenerAdapter {
   
   @Override
@@ -16,7 +19,11 @@ public class HelpCommand extends ListenerAdapter {
     String content = message.getContentRaw(); 
     MessageChannel channel = event.getChannel();
     if (content.equals(DenoriaBot.getPrefix() + "help")) {
-        channel.sendMessage("Menu pomocy!").queue();
+      EmbedBuilder embed = new EmbedBuilder();
+      embed.setTitle("Menu Pomocy Bota @DenoriaBot", null) 
+        .setColor(new Color(21, 242, 253))
+        .setDescription("Na razie bot jest w trakcie programowania,\n lecz mamy nadzieję że niedługo będzie tu więcej informacji!\n \n **Do zobaczenia w przyszłości!**");
+      channel.sendMessageEmbeds( embed.build() ).queue();
     }
   }
 }
